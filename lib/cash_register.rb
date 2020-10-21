de0efde81dfdd08358da9ed80f233c8207a5c808
Alla Kuzminova <alchik@gmail.com>
2020-10-21 18:03:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(employee_discount = nil)
+    @total = 0
+    @discount = employee_discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    self.total =+ self.total + price * quantity
+  end
+
+  def apply_discount
+    self.total = self.discount == nil ? self.total : self.total - (self.total * self.discount/100)
+    "After the discount, the total comes to $#{self.total}"
+  end
+
+end
